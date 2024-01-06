7ff14b4938d0aadead1d4c90bafc5362f818a6f8
Jorge Guzman <jorgeguzman2210@gmail.com>
2024-01-06 15:36:28 -0400

    add pytest

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,7 +1,7 @@
-def sum(num1:int, num2:int)-> int:
+def sum(num1:int, num2:int) -> int :
     return num1 + num2
 
 
-def test_sum_result():
+def test_sum_result() :
     """ #TODO: TOSTRING """
     assert sum(2, 2) == 4
